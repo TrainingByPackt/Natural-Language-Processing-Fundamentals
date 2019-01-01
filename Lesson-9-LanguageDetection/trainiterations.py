114cbdd0841654a87001c5331dfb8d0709b33663
Dwight Gunning <dgunning@gmail.com>
2019-01-01 13:58:55 -0500

    Add Lesson 9 files

---
+++ Lesson-9-LanguageDetection/trainiterations.py
@@ -0,0 +1,37 @@
+import time
+import math
+from randomchoices import randomTrainingExample
+
+n_iters = 60000
+print_every = 5000
+plot_every = 1000
+
+
+# Keep track of losses for plotting
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+
+    # Print iter number, loss, name and guess
+    if iter % print_every == 0:
+        guess, guess_i = languageFromOutput(output)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+
+    # Add current loss avg to list of losses
+    if iter % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
